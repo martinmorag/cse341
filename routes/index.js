b7cae51977fd755f5288b35864ac1e8451506d31
martinmorag <113954312+martinmorag@users.noreply.github.com>
2024-01-09 17:55:45 -0300

    Delete routes directory

--- routes/index.js
+++
@@ -1,7 +0,0 @@
-const routes = require('express').Router();
-const lesson1Controller = require('../controllers/lesson1');
-
-routes.get('/', lesson1Controller.worldRoute);
-routes.get('/name', lesson1Controller.nameRoute);
-
-module.exports = routes;
